65843912086825eb171c83ef8dc62947787013f2
Joskul <53687187+Joskul@users.noreply.github.com>
2023-10-11 22:23:51 +0700

    +New Grab/Release functions +Target selection system

--- main.ts
+++ main.ts
@@ -1,5 +1,22 @@
-function HeadForward () {
-    iBIT.Motor2(ibitMotor.Forward, 50 - deltaAngle(currentHeading), 50 + deltaAngle(currentHeading))
+/**
+ * Target
+ * 
+ * Red ball = target1
+ * 
+ * Blue ball = target2
+ * 
+ * Direction
+ * 
+ * left = 1
+ * 
+ * right = 2
+ */
+function HeadForward (speed: number) {
+    if (speed >= 0) {
+        iBIT.Motor2(ibitMotor.Forward, speed - deltaAngle(currentHeading), speed + deltaAngle(currentHeading))
+    } else {
+        iBIT.Motor2(ibitMotor.Backward, speed + deltaAngle(currentHeading), speed - deltaAngle(currentHeading))
+    }
     DrawCompass()
 }
 function DoTurn (turnAngle: number) {
@@ -25,6 +42,13 @@ function DoTurn (turnAngle: number) {
 input.onButtonPressed(Button.A, function () {
     Calibrate()
 })
+function Grab (targetObject: number) {
+    while (huskylens.isAppear(targetObject, HUSKYLENSResultType_t.HUSKYLENSResultBlock)) {
+        iBIT.Servo(ibitServo.SV1, 45)
+        HeadForward(baseSpeed)
+    }
+    iBIT.Servo(ibitServo.SV1, 0)
+}
 function DrawCompass () {
     tempDegrees = deltaAngle(currentHeading)
     serial.writeLine("" + (tempDegrees))
@@ -48,11 +72,37 @@ function Calibrate () {
 input.onButtonPressed(Button.AB, function () {
     input.calibrateCompass()
 })
-function Track () {
+function Track (targetObject: number, rightTurn: boolean) {
     while (!(leftIR() && rightIR())) {
-        HeadForward()
+        HeadForward(baseSpeed)
+        Grab(targetObject)
     }
+    Release()
+    if (rightTurn) {
+        DoTurn(90)
+    } else {
+        DoTurn(-90)
+    }
+    HeadForward(baseSpeed)
+    basic.pause(200)
+    if (rightTurn) {
+        DoTurn(90)
+    } else {
+        DoTurn(-90)
+    }
+    if (currentTarget == 1) {
+        currentTarget = 2
+    } else {
+        currentTarget = 1
+    }
+}
+function Release () {
+    HeadForward(baseSpeed)
+    basic.pause(200)
     iBIT.MotorStop()
+    iBIT.Servo(ibitServo.SV1, 45)
+    HeadForward(baseSpeed * -1)
+    basic.pause(200)
 }
 // returns angle (-180,180) positive being cw error
 // negative being ccw error
@@ -66,21 +116,23 @@ let tempDegrees = 0
 let tempD = 0
 let currentHeading = 0
 let calibrated = 0
+let currentTarget = 0
+let baseSpeed = 0
 let wbThreshold = 0
 huskylens.initI2c()
 huskylens.initMode(protocolAlgorithm.OBJECTCLASSIFICATION)
 wbThreshold = 256
+baseSpeed = 30
+currentTarget = 1
 calibrated = 0
 while (calibrated == 0) {
     basic.showIcon(IconNames.Diamond)
     basic.showIcon(IconNames.SmallDiamond)
 }
-HeadForward()
+HeadForward(baseSpeed)
 basic.pause(200)
 iBIT.MotorStop()
-basic.showIcon(IconNames.Yes)
-basic.pause(100)
-basic.clearScreen()
+basic.showIcon(IconNames.Happy)
 basic.forever(function () {
     DrawCompass()
 })
