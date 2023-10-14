ea7a532b994618fee3638f91fa8d10de0b581599
Joskul <53687187+Joskul@users.noreply.github.com>
2023-10-14 22:43:13 +0700

    +Minor optimization +Reduce code redundancy

--- main.ts
+++ main.ts
@@ -5,35 +5,48 @@
  * 
  * Blue ball = target2
  */
-function HeadForward (speed: number) {
-    if (speed >= 0) {
-        iBIT.Motor2(ibitMotor.Forward, speed - deltaAngle(currentHeading), speed + deltaAngle(currentHeading))
+function HeadForward (speed: number, duration: number) {
+    if (duration <= 1) {
+        if (speed >= 0) {
+            iBIT.Motor2(ibitMotor.Forward, speed - deltaAngle(), speed + deltaAngle())
+        } else {
+            iBIT.Motor2(ibitMotor.Backward, Math.abs(speed) + deltaAngle(), Math.abs(speed) - deltaAngle())
+        }
+        Log()
     } else {
-        iBIT.Motor2(ibitMotor.Backward, speed + deltaAngle(currentHeading), speed - deltaAngle(currentHeading))
+        tempT = input.runningTime()
+        while (input.runningTime() - tempT < duration) {
+            if (speed >= 0) {
+                iBIT.Motor2(ibitMotor.Forward, speed - deltaAngle(), speed + deltaAngle())
+            } else {
+                iBIT.Motor2(ibitMotor.Backward, Math.abs(speed) + deltaAngle(), Math.abs(speed) - deltaAngle())
+            }
+            Log()
+        }
     }
-    DrawCompass()
 }
 function DoTurn (turnAngle: number) {
-    tempD = (currentHeading + turnAngle) % 360
-    if (tempD < 0) {
-        currentHeading = tempD + 360
-    } else {
-        currentHeading = tempD
-    }
-    tempD = deltaAngle(currentHeading)
+    currentHeading = (currentHeading + turnAngle) % 360
+    tempD = deltaAngle()
     while (Math.abs(tempD) > angleThreshold) {
-        tempD = deltaAngle(currentHeading)
+        tempD = deltaAngle()
         if (tempD > angleThreshold) {
-            iBIT.setMotor(ibitMotorCH.M1, ibitMotor.Backward, 15 + tempD)
-            iBIT.setMotor(ibitMotorCH.M2, ibitMotor.Forward, 15 + tempD)
-        } else if (tempD < angleThreshold * -1) {
-            iBIT.setMotor(ibitMotorCH.M1, ibitMotor.Forward, 15 - tempD)
-            iBIT.setMotor(ibitMotorCH.M2, ibitMotor.Backward, 15 - tempD)
+            iBIT.setMotor(ibitMotorCH.M1, ibitMotor.Backward, Math.min(Math.max(0 + tempD, minimumTurnSpeed), maximumTurnSpeed))
+            iBIT.setMotor(ibitMotorCH.M2, ibitMotor.Forward, Math.min(Math.max(0 + tempD, minimumTurnSpeed), maximumTurnSpeed))
         } else {
-            iBIT.MotorStop()
+            iBIT.setMotor(ibitMotorCH.M1, ibitMotor.Forward, Math.min(Math.max(0 - tempD, minimumTurnSpeed), maximumTurnSpeed))
+            iBIT.setMotor(ibitMotorCH.M2, ibitMotor.Backward, Math.min(Math.max(0 - tempD, minimumTurnSpeed), maximumTurnSpeed))
         }
-        DrawCompass()
+        Log()
     }
+    iBIT.MotorStop()
+}
+function test3 () {
+    HeadForward(baseSpeed, 2000)
+    DoTurn(90)
+}
+function test1 () {
+    DoTurn(0)
 }
 input.onButtonPressed(Button.A, function () {
     Calibrate()
@@ -46,9 +59,8 @@ function Grab () {
         iBIT.Servo(ibitServo.SV1, 0)
     }
 }
-function DrawCompass () {
-    tempDegrees = deltaAngle(currentHeading)
-    serial.writeLine("" + (tempDegrees))
+function test2 () {
+    HeadForward(baseSpeed, 1)
 }
 function rightIR () {
     return iBIT.ReadADC(ibitReadADC.ADC0) < wbThreshold
@@ -65,13 +77,17 @@ function Calibrate () {
     }
     currentHeading = input.compassHeading()
     calibrated = 1
+    while (leftIR() && rightIR()) {
+        iBIT.Motor(ibitMotor.Forward, baseSpeed)
+    }
+    basic.pause(1000)
 }
 input.onButtonPressed(Button.AB, function () {
     input.calibrateCompass()
 })
 function Track (rightTurn: boolean) {
     while (!(leftIR() && rightIR())) {
-        HeadForward(baseSpeed)
+        HeadForward(baseSpeed, 0)
         Grab()
     }
     Release()
@@ -80,8 +96,7 @@ function Track (rightTurn: boolean) {
     } else {
         DoTurn(-90)
     }
-    HeadForward(baseSpeed)
-    basic.pause(200)
+    HeadForward(baseSpeed, 500)
     if (rightTurn) {
         DoTurn(90)
     } else {
@@ -93,26 +108,33 @@ function Track (rightTurn: boolean) {
         currentTarget = 1
     }
 }
+input.onButtonPressed(Button.B, function () {
+    DoTurn(90)
+})
+function Log () {
+	
+}
 function Release () {
-    HeadForward(baseSpeed)
-    basic.pause(200)
+    HeadForward(baseSpeed, 1000)
     iBIT.MotorStop()
     iBIT.Servo(ibitServo.SV1, 45)
-    HeadForward(baseSpeed * -1)
-    basic.pause(200)
+    HeadForward(baseSpeed * -1, 1500)
+    Calibrate()
 }
 // returns angle (-180,180) positive being cw error
 // negative being ccw error
-function deltaAngle (targetAngle: number) {
-    return (input.compassHeading() - targetAngle + 180) % 360 - 180
+function deltaAngle () {
+    return (input.compassHeading() - currentHeading + 180) % 360 - 180
 }
 function leftIR () {
     return iBIT.ReadADC(ibitReadADC.ADC1) < wbThreshold
 }
-let tempDegrees = 0
 let tempD = 0
 let currentHeading = 0
+let tempT = 0
 let calibrated = 0
+let maximumTurnSpeed = 0
+let minimumTurnSpeed = 0
 let angleThreshold = 0
 let currentTarget = 0
 let baseSpeed = 0
@@ -122,16 +144,15 @@ huskylens.initMode(protocolAlgorithm.ALGORITHM_COLOR_RECOGNITION)
 wbThreshold = 256
 baseSpeed = 30
 currentTarget = 1
-angleThreshold = 1
+angleThreshold = 3
+minimumTurnSpeed = 2
+maximumTurnSpeed = 20
 calibrated = 0
 while (calibrated == 0) {
     basic.showIcon(IconNames.Diamond)
     basic.showIcon(IconNames.SmallDiamond)
 }
-HeadForward(baseSpeed)
-basic.pause(200)
-iBIT.MotorStop()
 basic.showIcon(IconNames.Happy)
 basic.forever(function () {
-    DoTurn(0)
+    test3()
 })
