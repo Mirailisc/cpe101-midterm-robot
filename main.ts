02b7383b19ec2cf8519360bbd2dc39241441da1d
Joskul <53687187+Joskul@users.noreply.github.com>
2023-10-17 10:30:51 +0700

    +Dumb version for dumb demonstration

--- main.ts
+++ main.ts
@@ -4,42 +4,23 @@
 // 
 // Blue ball = target2
 function HeadForward (speed: number, duration: number) {
-    tempV = Math.max(Math.min(deltaAngle() * 1, 30), -30)
-    if (duration <= 1) {
-        if (speed >= 0) {
-            iBIT.Motor2(ibitMotor.Forward, speed - tempV, speed + tempV)
-        } else {
-            iBIT.Motor2(ibitMotor.Backward, Math.abs(speed) + tempV, Math.abs(speed) - tempV)
-        }
-        Log()
+    if (speed >= 0) {
+        iBIT.Motor(ibitMotor.Forward, speed)
     } else {
-        tempT = input.runningTime()
-        while (input.runningTime() - tempT < duration) {
-            tempV = Math.max(Math.min(deltaAngle() * 1, 30), -30)
-            if (speed >= 0) {
-                iBIT.Motor2(ibitMotor.Forward, speed - tempV, speed + tempV)
-            } else {
-                iBIT.Motor2(ibitMotor.Backward, Math.abs(speed) + tempV, Math.abs(speed) - tempV)
-            }
-            Log()
-        }
+        iBIT.Motor(ibitMotor.Backward, Math.abs(speed))
     }
+    if (duration > 1) {
+        basic.pause(duration)
+    }
+    iBIT.MotorStop()
 }
 function DoTurn (turnAngle: number) {
-    currentHeading = (currentHeading + turnAngle) % 360
-    tempD = deltaAngle()
-    while (Math.abs(tempD) > angleThreshold) {
-        tempD = deltaAngle()
-        tempV = Math.min(Math.max(0 + Math.abs(tempD), minimumTurnSpeed), maximumTurnSpeed)
-        if (tempD > angleThreshold) {
-            iBIT.setMotor(ibitMotorCH.M1, ibitMotor.Backward, tempV)
-            iBIT.setMotor(ibitMotorCH.M2, ibitMotor.Forward, tempV)
-        } else {
-            iBIT.setMotor(ibitMotorCH.M1, ibitMotor.Forward, tempV)
-            iBIT.setMotor(ibitMotorCH.M2, ibitMotor.Backward, tempV)
-        }
-        Log()
+    if (turnAngle < 0) {
+        iBIT.Spin(ibitSpin.Left, Math.abs(turnSpeed))
+    } else {
+        iBIT.Spin(ibitSpin.Right, turnSpeed)
     }
+    basic.pause(turnAngle * 2)
     iBIT.MotorStop()
 }
 function MainLoop () {
@@ -131,10 +112,10 @@ function Log () {
     radio.sendValue("dAng", tempDegrees)
 }
 function Release () {
-    HeadForward(baseSpeed, 1500)
+    HeadForward(20, 1500)
     iBIT.MotorStop()
     iBIT.Servo(ibitServo.SV1, 45)
-    HeadForward(baseSpeed * -1, 1000)
+    HeadForward(-20, 1000)
 }
 function Test1 () {
     DoTurn(0)
@@ -148,15 +129,10 @@ function leftIR () {
     return iBIT.ReadADC(ibitReadADC.ADC1) < wbThreshold
 }
 let tempDegrees = 0
-let tempD = 0
 let currentHeading = 0
-let tempT = 0
-let tempV = 0
 let calibrated = 0
-let maximumTurnSpeed = 0
-let minimumTurnSpeed = 0
-let angleThreshold = 0
 let currentTarget = 0
+let turnSpeed = 0
 let baseSpeed = 0
 let wbThreshold = 0
 radio.setGroup(69)
@@ -164,10 +140,8 @@ huskylens.initI2c()
 huskylens.initMode(protocolAlgorithm.ALGORITHM_COLOR_RECOGNITION)
 wbThreshold = 256
 baseSpeed = 30
+turnSpeed = 20
 currentTarget = 1
-angleThreshold = 3
-minimumTurnSpeed = 5
-maximumTurnSpeed = 30
 calibrated = 0
 while (calibrated == 0) {
     basic.showIcon(IconNames.Diamond)
